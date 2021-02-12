4b9c63ea3e20b383353ee8cbe6309f7892806641
Azhar <33951589+qctfw@users.noreply.github.com>
2021-02-12 10:33:02 +0700

    Redirecting Requests (Lecture No. 33)

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     if (url === '/') {
         res.setHeader('Content-Type', 'text/html');
@@ -9,7 +11,12 @@ const server = http.createServer((req, res) => {
         res.write('<head><title>Send Message</title></head>');
         res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
         res.write('</html>');
-        
+        return res.end();
+    }
+    if (url === '/message' && method == 'POST') {
+        fs.writeFileSync('message.txt', 'DUMMY');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
         return res.end();
     }
 
