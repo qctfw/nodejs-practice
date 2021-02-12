0b4ddac9e528a4b8204404410fd6160c6957fa47
Azhar <33951589+qctfw@users.noreply.github.com>
2021-02-12 10:22:21 +0700

    Routing Requests (Lecture No. 32)

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
+
+    if (url === '/') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>Send Message</title></head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
