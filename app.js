08860bf89caf14310999ed0be07de5e642a5e363
Azhar <33951589+qctfw@users.noreply.github.com>
2021-02-12 10:07:43 +0700

    Sending Response (Lecture No. 30)

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log(req);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Hello from My Node.js Server!</h1></body>');
+    res.write('</html>');
+
+    res.end();
 });
 
 server.listen(3000);
