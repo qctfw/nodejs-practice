ab62074805c174745d1290c3a98eaf15e11e2c3a
Azhar <33951589+qctfw@users.noreply.github.com>
2021-04-15 14:02:52 +0700

    Serving HTML Pages (Lecture No. 70)

--- routes/shop.js
+++ routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 const express = require('express');
 
 const Router = express.Router();
 
 Router.get('/', (req, res, next) => {
-    res.send('<h1>Hello from Express!</h1>');
+    res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'));
 });
 
 module.exports = Router;
