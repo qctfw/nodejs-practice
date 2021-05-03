c1baea0c12d0c2e7bb10d9dc59e3e7deeac9e660
Azhar <33951589+qctfw@users.noreply.github.com>
2021-05-03 13:36:28 +0700

    Linking to the Edit Page (Lecture No. 126)

--- routes/admin.js
+++ routes/admin.js
@@ -19,4 +19,7 @@ router.post('/add-product', adminController.postAddProduct);
 // /admin/edit-product/:productId => GET
 router.get('/edit-product/:productId', adminController.getEditProduct);
 
+// /admin/edit-product => POST
+router.post('/edit-product', adminController.postEditProduct);
+
 module.exports = router;
