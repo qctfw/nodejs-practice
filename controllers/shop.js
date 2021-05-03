aa0beeb9645045e88e95bf2776f949ce770262ba
Azhar <33951589+qctfw@users.noreply.github.com>
2021-05-03 12:01:57 +0700

    Passing Data with POST Requests (Lecture No. 122)

--- controllers/shop.js
+++ controllers/shop.js
@@ -38,6 +38,12 @@ exports.getCart = (req, res, next) => {
     });
 }
 
+exports.postCart = (req, res, next) => {
+    const productId = req.body.productId;
+    console.log(productId);
+    res.redirect('/cart');
+}
+
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
         pageTitle: 'Your Orders',
