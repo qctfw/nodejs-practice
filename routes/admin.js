8fc5d184ef00d9b26570ffa52c2e7176c8b80511
Azhar <33951589+qctfw@users.noreply.github.com>
2021-05-03 13:22:19 +0700

    Using Query Params (Lecture No. 124)

--- routes/admin.js
+++ routes/admin.js
@@ -16,4 +16,7 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+// /admin/edit-product/:productId => GET
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
 module.exports = router;
