9a90d8492ab8c6c5130294f045254e0f383d36a5
Azhar <33951589+qctfw@users.noreply.github.com>
2021-05-05 14:57:28 +0700

    Getting a Single Product with the "where" Condition (Lecture No. 157)

--- controllers/shop.js
+++ controllers/shop.js
@@ -15,15 +15,15 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
     const productId = req.params.productId;
-    Product.findById(productId)
-    .then(([product, fieldData]) => {
-        res.render('shop/product-detail', {
-            product: product[0],
-            pageTitle: product.title,
-            path: '/products'
-        });
-    })
-    .catch(err => console.log(err));
+    Product.findByPk(productId)
+        .then((product) => {
+            res.render('shop/product-detail', {
+                product: product,
+                pageTitle: product.title,
+                path: '/products'
+            });
+        })
+        .catch(err => console.log(err));
 };
 
 exports.getIndex = (req, res, next) => {
